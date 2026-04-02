e9a82051854222e1cf349dc73ba1344eb3464dd2
NhanVo <hoangnhanvo17@gmail.com>
2026-04-02 02:06:07 -0500

    fix PrismaLibSql casing and remove turbopack.root

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  turbopack: {
-    root: "c:\\Users\\h263864\\KPI Project\\kpi-report",
-  },
 };
 
 export default nextConfig;
